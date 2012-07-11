4fde72312fe2f053241b6b7dac8e69d2f102e5e4
tmortagne <thomas.mortagne@gmail.com>
2012-07-11 18:37:38 +0200

    XCOMMONS-223: Extension installed on a namespace is lost when restarting Fix merge issue

--- xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/repository/internal/local/DefaultExtensionSerializer.java
+++ xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/repository/internal/local/DefaultExtensionSerializer.java
@@ -28,8 +28,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
