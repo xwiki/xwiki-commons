714077d18e7e92aa4dddfcb0c542607681abe376
tmortagne <thomas.mortagne@gmail.com>
2012-11-02 15:10:33 +0100

    XCOMMONS-285: Add a java.util.Locale property converter

---
+++ xwiki-commons-core/xwiki-commons-properties/src/main/java/org/xwiki/properties/internal/converter/LocaleConverter.java
@@ -0,0 +1,53 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.properties.internal.converter;
+
+import java.lang.reflect.Type;
+import java.util.Locale;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.LocaleUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.properties.converter.AbstractConverter;
+
+/**
+ * Bean Utils converter that converts a value into an {@link Locale} object.
+ * 
+ * @version $Id$
+ * @since 4.3M2
+ */
+@Component
+@Named("java.util.Locale")
+@Singleton
+public class LocaleConverter extends AbstractConverter
+{
+    @Override
+    protected Locale convertToType(Type type, Object value)
+    {
+        Locale locale = null;
+        if (value != null) {
+            locale = LocaleUtils.toLocale(value.toString());
+        }
+
+        return locale;
+    }
+}
