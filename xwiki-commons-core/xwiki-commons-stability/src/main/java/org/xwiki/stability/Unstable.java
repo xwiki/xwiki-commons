f0635dcfa624085d659bb6a007af937afbf19140
Vincent Massol <vincent@massol.net>
2013-02-23 11:33:31 +0100

    XCOMMONS-352: Introduce new @Unstable annotation + start using it for the 4 methods I've added yesterday in ModelScriptService

---
+++ xwiki-commons-core/xwiki-commons-stability/src/main/java/org/xwiki/stability/Unstable.java
@@ -0,0 +1,48 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.stability;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Indicates that a java type (class, interface, method, etc) API is still unstable and subject to change at any time.
+ * This means that the XWiki deprecation policy doesn't apply yet on the annotated type.
+ *
+ * @version $Id$
+ * @since 5.0M1
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+@Inherited
+public @interface Unstable
+{
+    /**
+     * An optional explanation about the reason why the java type is unstable.
+     */
+    String value() default "";
+}
