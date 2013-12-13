6696d3e701031ce855d7ea2d16831ba470dc6a29
tmortagne <thomas.mortagne@gmail.com>
2013-12-13 17:24:52 +0100

    XCOMMONS-500: Properties related informations should be inheritable

--- xwiki-commons-core/xwiki-commons-properties/src/main/java/org/xwiki/properties/annotation/PropertyName.java
+++ xwiki-commons-core/xwiki-commons-properties/src/main/java/org/xwiki/properties/annotation/PropertyName.java
@@ -20,6 +20,7 @@
 package org.xwiki.properties.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -32,6 +33,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD })
+@Inherited
 public @interface PropertyName
 {
     /**
