10b66057ac982b7c1ecb7199c240718af822695d
Vincent Massol <vincent@massol.net>
2014-03-20 15:49:57 +0100

    [Misc] Add missing javadoc + reorder to have tests first

--- xwiki-commons-core/xwiki-commons-xml/src/test/java/org/xwiki/xml/stax/StAXUtilsTests.java
+++ xwiki-commons-core/xwiki-commons-xml/src/test/java/org/xwiki/xml/stax/StAXUtilsTests.java
@@ -44,39 +44,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+/**
+ * Unit tests for {@link org.xwiki.xml.stax.StAXUtils}.
+ *
+ * @version $Id$
+ * @since 5.2M1
+ */
 public class StAXUtilsTests
 {
-    private XMLStreamReader mockXMLStreamReader()
-    {
-        XMLStreamReader xmlStreamReader = Mockito.mock(XMLStreamReader.class);
-
-        Mockito.when(xmlStreamReader.getEventType()).thenReturn(XMLStreamConstants.START_DOCUMENT);
-
-        Location location = Mockito.mock(Location.class);
-
-        Mockito.when(xmlStreamReader.getLocation()).thenReturn(location);
-
-        return xmlStreamReader;
-    }
-
-    private XMLEventReader mockXMLEventReader() throws XMLStreamException
-    {
-        XMLEventReader xmlEventReader = Mockito.mock(XMLEventReader.class);
-
-        XMLEvent event = Mockito.mock(XMLEvent.class);
-        Mockito.when(event.getEventType()).thenReturn(XMLStreamConstants.START_DOCUMENT);
-
-        Mockito.when(xmlEventReader.peek()).thenReturn(event);
-
-        Location location = Mockito.mock(Location.class);
-
-        Mockito.when(event.getLocation()).thenReturn(location);
-
-        return xmlEventReader;
-    }
-
-    // tests
-
     @Test
     public void testGetXMLStreamReader() throws XMLStreamException
     {
@@ -126,5 +101,34 @@ public class StAXUtilsTests
 
         Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "<element attribute=\"value\">"
             + "characters" + "<![CDATA[cdata]]>" + "</element>", output.getBuffer().toString());
+    }
+
+    private XMLStreamReader mockXMLStreamReader()
+    {
+        XMLStreamReader xmlStreamReader = Mockito.mock(XMLStreamReader.class);
+
+        Mockito.when(xmlStreamReader.getEventType()).thenReturn(XMLStreamConstants.START_DOCUMENT);
+
+        Location location = Mockito.mock(Location.class);
+
+        Mockito.when(xmlStreamReader.getLocation()).thenReturn(location);
+
+        return xmlStreamReader;
+    }
+
+    private XMLEventReader mockXMLEventReader() throws XMLStreamException
+    {
+        XMLEventReader xmlEventReader = Mockito.mock(XMLEventReader.class);
+
+        XMLEvent event = Mockito.mock(XMLEvent.class);
+        Mockito.when(event.getEventType()).thenReturn(XMLStreamConstants.START_DOCUMENT);
+
+        Mockito.when(xmlEventReader.peek()).thenReturn(event);
+
+        Location location = Mockito.mock(Location.class);
+
+        Mockito.when(event.getLocation()).thenReturn(location);
+
+        return xmlEventReader;
     }
 }
