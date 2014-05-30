388db63a6eed10b8e3a07634802ef8e164be1744
tmortagne <thomas.mortagne@gmail.com>
2014-05-30 11:04:19 +0200

    XCOMMONS-599: Add converter for java.lang.reflect.Type

---
+++ xwiki-commons-core/xwiki-commons-properties/src/main/java/org/xwiki/properties/internal/converter/TypeConverter.java
@@ -0,0 +1,56 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.properties.internal.converter;
+
+import java.lang.reflect.Type;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.util.ReflectionUtils;
+import org.xwiki.properties.converter.AbstractConverter;
+import org.xwiki.properties.converter.ConversionException;
+
+/**
+ * Converter that converts a value into an {@link Type} object.
+ * 
+ * @version $Id$
+ * @since 6.1M1
+ */
+@Component
+@Singleton
+public class TypeConverter extends AbstractConverter<Type>
+{
+    @Override
+    protected Type convertToType(Type type, Object value)
+    {
+        Type result = null;
+        if (value != null) {
+            String valueString = value.toString();
+            try {
+                result = ReflectionUtils.unserializeType(valueString, Thread.currentThread().getContextClassLoader());
+            } catch (ClassNotFoundException e) {
+                throw new ConversionException("Invalid Type [" + valueString + "]", e);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ xwiki-commons-core/xwiki-commons-properties/src/test/java/org/xwiki/properties/internal/converter/TypeConverterTest.java
@@ -0,0 +1,74 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.properties.internal.converter;
+
+import java.lang.reflect.Type;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.xwiki.component.descriptor.ComponentRole;
+import org.xwiki.component.util.DefaultParameterizedType;
+import org.xwiki.properties.ConverterManager;
+import org.xwiki.test.jmock.AbstractComponentTestCase;
+
+/**
+ * Validate {@link LocaleConverter} component.
+ * 
+ * @version $Id$
+ */
+public class TypeConverterTest extends AbstractComponentTestCase
+{
+    private ConverterManager converterManager;
+
+    @Before
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        this.converterManager = getComponentManager().getInstance(ConverterManager.class);
+    }
+
+    @Test
+    public void testConvertToType()
+    {
+        Type simpleType = ComponentRole.class;
+        Assert.assertEquals(simpleType,
+            this.converterManager.convert(Type.class, "org.xwiki.component.descriptor.ComponentRole"));
+
+        Type genericsType = new DefaultParameterizedType(null, ComponentRole.class, String.class);
+        Assert
+            .assertEquals(genericsType, this.converterManager.convert(Type.class,
+                "org.xwiki.component.descriptor.ComponentRole< java.lang.String >"));
+    }
+
+    @Test
+    public void testConvertToString()
+    {
+        Type simpleType = ComponentRole.class;
+        Assert.assertEquals("org.xwiki.component.descriptor.ComponentRole",
+            this.converterManager.convert(String.class, simpleType));
+
+        Type genericsType = new DefaultParameterizedType(null, ComponentRole.class, String.class);
+        Assert.assertEquals("org.xwiki.component.descriptor.ComponentRole< java.lang.String >",
+            this.converterManager.convert(String.class, genericsType));
+    }
+}
