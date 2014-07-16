394553b19e87aaeb951e9faa15ee110555e995f1
Eduard Moraru <Enygma2002@gmail.com>
2014-07-16 17:47:32 +0300

    XWIKI-10545: Use the snapshots extension repository by default for snapshot builds - Created an ExtensionRepositorySource for the snapshot repository to enable it by default

---
+++ xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-repositories/xwiki-commons-extension-repository-maven-snapshots/src/main/java/org/xwiki/extension/repository/aether/internal/AetherSnapshotsDefaultExtensionRepositorySource.java
@@ -0,0 +1,70 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.extension.repository.aether.internal;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.extension.ExtensionManagerConfiguration;
+import org.xwiki.extension.repository.AbstractExtensionRepositorySource;
+import org.xwiki.extension.repository.DefaultExtensionRepositoryDescriptor;
+import org.xwiki.extension.repository.ExtensionRepositoryDescriptor;
+
+/**
+ * Provides the XWiki snapshots repository as default maven repository.
+ * 
+ * @version $Id$
+ * @since 6.2M1
+ */
+@Component
+@Singleton
+@Named("default.aether.snapshots")
+public class AetherSnapshotsDefaultExtensionRepositorySource extends AbstractExtensionRepositorySource
+{
+    /**
+     * Used to get configuration properties containing repositories.
+     */
+    @Inject
+    private ExtensionManagerConfiguration configuration;
+
+    @Override
+    public Collection<ExtensionRepositoryDescriptor> getExtensionRepositoryDescriptors()
+    {
+        Collection<ExtensionRepositoryDescriptor> configuredRepositories =
+            this.configuration.getExtensionRepositoryDescriptors();
+
+        Collection<ExtensionRepositoryDescriptor> newRepositories = new ArrayList<ExtensionRepositoryDescriptor>();
+
+        if (CollectionUtils.isEmpty(configuredRepositories)
+            && !"true".equals(System.getProperty("skipSnapshotModules"))) {
+            newRepositories.add(new DefaultExtensionRepositoryDescriptor("maven-xwiki-snapshot", "maven", URI
+                .create("http://nexus.xwiki.org/nexus/content/groups/public-snapshots")));
+        }
+
+        return newRepositories;
+    }
+}
