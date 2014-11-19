1869268433c86d996835cb7fea1597c82570c94b
Vincent Massol <vincent@massol.net>
2014-11-19 19:41:22 +0100

    XCOMMONS-692: Automatically check content of components.txt files  * Moved Annotation Processor into existing verification-resources module since it's about verifying stuff...

--- xwiki-commons-tools/xwiki-commons-tool-verification-resources/src/main/java/org/xwiki/tool/component/ComponentCheckerAnnotationProcessor.java
+++ xwiki-commons-tools/xwiki-commons-tool-verification-resources/src/main/java/org/xwiki/tool/component/ComponentCheckerAnnotationProcessor.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.tool.component.internal;
+package org.xwiki.tool.component;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
