1fde18384d4638dbf06c9133a5fcbf1c8d62fa24
tmortagne <thomas.mortagne@gmail.com>
2015-01-20 10:45:15 +0100

    XCOMMONS-723: Impossible to use a Converter installed in an extension * fix test
    (cherry picked from commit d667c231d17775efcf3328811e4c73716b5c6d12)
    
    Conflicts:
    	xwiki-commons-core/xwiki-commons-properties/src/test/java/org/xwiki/properties/internal/DefaultBeanManagerTest.java

--- xwiki-commons-core/xwiki-commons-properties/src/test/java/org/xwiki/properties/internal/DefaultBeanManagerTest.java
+++ xwiki-commons-core/xwiki-commons-properties/src/test/java/org/xwiki/properties/internal/DefaultBeanManagerTest.java
@@ -26,6 +26,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.xwiki.component.internal.ContextComponentManagerProvider;
 import org.xwiki.properties.BeanManager;
 import org.xwiki.properties.PropertyException;
 import org.xwiki.properties.PropertyMandatoryException;
@@ -41,8 +42,8 @@ import org.xwiki.test.annotation.ComponentList;
  * 
  * @version $Id$
  */
-@ComponentList({DefaultBeanManager.class, DefaultConverterManager.class, EnumConverter.class,
-ConvertUtilsConverter.class})
+@ComponentList({ DefaultBeanManager.class, DefaultConverterManager.class, EnumConverter.class,
+    ConvertUtilsConverter.class, ContextComponentManagerProvider.class })
 public class DefaultBeanManagerTest
 {
     public static class RawPropertiesTest extends HashMap<String, Object> implements RawProperties
