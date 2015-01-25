be102fbd98ef77cd8cc629ccf497dada00429ac3
Vincent Massol <vincent@massol.net>
2015-01-25 13:42:45 +0100

    XCOMMONS-730: Fail the build when the @Unstable annotation must be removed XCOMMONS-731: Fail the build if an @Unstable annotation is added without @since javadoc tag

---
+++ xwiki-commons-tools/xwiki-commons-tool-verification-resources/src/main/java/org/xwiki/tool/checkstyle/UnstableAnnotationCheck.java
@@ -0,0 +1,153 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.tool.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.api.AnnotationUtility;
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FullIdent;
+import com.puppycrawl.tools.checkstyle.api.TextBlock;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * Verify if Unstable annotations should be removed.
+ *
+ * @version $Id$
+ * @since 7.0M1
+ */
+public class UnstableAnnotationCheck extends Check
+{
+    private String packageName;
+
+    private String classOrInterfaceName;
+
+    private String currentVersion;
+
+    private int currentVersionMajor;
+
+    @Override
+    public int[] getDefaultTokens()
+    {
+        return new int[]{
+            TokenTypes.PACKAGE_DEF, TokenTypes.INTERFACE_DEF, TokenTypes.CLASS_DEF, TokenTypes.METHOD_DEF
+        };
+    }
+
+    public void setCurrentVersion(String currentVersion) throws CheckstyleException
+    {
+        this.currentVersion = currentVersion;
+        this.currentVersionMajor = extractMajor(currentVersion);
+        if (this.currentVersionMajor == -1) {
+            throw new CheckstyleException("The passed version [" + this.currentVersionMajor
+                + "] must be of the type Major.* (e.g. 7.0-SNAPSHOT)");
+        }
+    }
+
+    @Override
+    public void visitToken(DetailAST ast)
+    {
+        switch (ast.getType()) {
+            case TokenTypes.PACKAGE_DEF:
+                // Save the package
+                FullIdent ident = FullIdent.createFullIdent(ast.getLastChild().getPreviousSibling());
+                this.packageName = ident.getText();
+                break;
+            case TokenTypes.CLASS_DEF:
+            case TokenTypes.INTERFACE_DEF:
+                this.classOrInterfaceName = ast.findFirstToken(TokenTypes.IDENT).getText();
+                break;
+        }
+
+        if (AnnotationUtility.containsAnnotation(ast)) {
+            DetailAST holder = AnnotationUtility.getAnnotationHolder(ast);
+            DetailAST annotation = getAnnotationAst(holder);
+            String annotationName = annotation.findFirstToken(TokenTypes.IDENT).getText();
+            if (annotationName.equals("Unstable")) {
+                FileContents contents = getFileContents();
+                TextBlock cmt = contents.getJavadocBefore(ast.getLineNo());
+                String annotatedElementName = ast.findFirstToken(TokenTypes.IDENT).getText();
+                String sinceVersion = extractSinceVersionFromJavadoc(cmt.getText());
+                int sinceMajor = extractMajor(sinceVersion);
+                if (sinceMajor == -1) {
+                    log(annotation.getLineNo(), annotation.getColumnNo(), String.format("The @since version [%s] "
+                        + "] must be of the type Major.* (e.g. 7.0-SNAPSHOT)", sinceVersion));
+                    return;
+                } else {
+                    // Log an error the @Unstable annotation has been there for more than a full cycle!
+                    if (this.currentVersionMajor - 2 >= sinceMajor) {
+                        log(annotation.getLineNo(), annotation.getColumnNo(), String.format("The @Unstable annotation "
+                            + "for [%s.%s%s] must be removed since it's been there for more than a full "
+                            + "development cycle (was introduced in [%s] and current version is [%s])",
+                            this.packageName, this.classOrInterfaceName,
+                            annotatedElementName.equals(this.classOrInterfaceName) ? "" : "." + annotatedElementName
+                            + "()", sinceVersion, this.currentVersion));
+                    }
+                }
+            }
+        }
+    }
+
+    private String extractSinceVersionFromJavadoc(String[] javadocLines)
+    {
+        String sinceVersion = null;
+        for (String javadocLine : javadocLines) {
+            int pos = javadocLine.indexOf("@since");
+            if (pos > -1) {
+                sinceVersion = javadocLine.substring(pos + "@since".length() + 1);
+                break;
+            }
+        }
+        return sinceVersion;
+    }
+
+    private static DetailAST getAnnotationAst(DetailAST aHolderAst)
+    {
+        if (aHolderAst.getType() == TokenTypes.ANNOTATIONS) {
+            return aHolderAst;
+        } else if (aHolderAst.getType() == TokenTypes.MODIFIERS) {
+            return aHolderAst.findFirstToken(TokenTypes.ANNOTATION);
+        }
+        throw new AssertionError("aHolder must be one of TokenTypes.ANNOTATIONS or TokenTypes.MODIFIERS but was "
+            + aHolderAst);
+    }
+
+    private int extractMajor(String version)
+    {
+        if (version == null) {
+            return -1;
+        }
+
+        int major;
+        boolean hasError = false;
+        int pos = version.indexOf(".");
+        if (pos > -1) {
+            try {
+                major = Integer.parseInt(version.substring(0, pos));
+            } catch (NumberFormatException e) {
+                major = -1;
+            }
+        } else {
+            major = -1;
+        }
+        return major;
+    }
+}
