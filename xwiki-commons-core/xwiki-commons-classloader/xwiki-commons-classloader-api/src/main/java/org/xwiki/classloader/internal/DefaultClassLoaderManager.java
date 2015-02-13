6252271711def4f7c20414774fb51a93ecbc35c5
tmortagne <thomas.mortagne@gmail.com>
2015-02-13 17:16:49 +0100

    XCOMMONS-750: DefaultClassLoaderManager loose the container classloader

--- xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/main/java/org/xwiki/classloader/internal/DefaultClassLoaderManager.java
+++ xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/main/java/org/xwiki/classloader/internal/DefaultClassLoaderManager.java
@@ -28,6 +28,8 @@ import javax.inject.Singleton;
 import org.xwiki.classloader.ClassLoaderManager;
 import org.xwiki.classloader.NamespaceURLClassLoader;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.phase.Initializable;
+import org.xwiki.component.phase.InitializationException;
 
 /**
  * Default implementation of {@link ClassLoaderManager}.
@@ -37,7 +39,7 @@ import org.xwiki.component.annotation.Component;
  */
 @Component
 @Singleton
-public class DefaultClassLoaderManager implements ClassLoaderManager
+public class DefaultClassLoaderManager implements ClassLoaderManager, Initializable
 {
     /**
      * The class loader corresponding to null namespace.
@@ -50,23 +52,16 @@ public class DefaultClassLoaderManager implements ClassLoaderManager
     private Map<String, NamespaceURLClassLoader> wikiClassLoaderMap =
         new ConcurrentHashMap<String, NamespaceURLClassLoader>();
 
-    /**
-     * Allow overriding the system classloader during tests.
-     *
-     * @return a ClassLoader to be used as the system parent
-     */
-    protected ClassLoader getSystemClassLoader()
+    @Override
+    public void initialize() throws InitializationException
     {
-        return getClass().getClassLoader();
+        this.rootClassLoader =
+            new NamespaceURLClassLoader(new URI[] {}, Thread.currentThread().getContextClassLoader(), null);
     }
 
     @Override
     public NamespaceURLClassLoader getURLClassLoader(String namespace, boolean create)
     {
-        if (this.rootClassLoader == null) {
-            this.rootClassLoader = new NamespaceURLClassLoader(new URI[] {}, getSystemClassLoader(), null);
-        }
-
         NamespaceURLClassLoader wikiClassLoader = this.rootClassLoader;
 
         if (namespace != null) {
