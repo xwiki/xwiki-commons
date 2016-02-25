85b03a78f31aa80e843ce7bb2161916485a61c3f
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-02-25 18:04:41 +0100

    XCOMMONS-923: URLClassloader#close should be called when dropping a classloader XCOMMONS-922: Classloader corresponding to root namespace is never dropped
    (cherry picked from commit 04c645a5574ec7f37112df4300480515c9011a6e)

--- xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/main/java/org/xwiki/classloader/internal/DefaultClassLoaderManager.java
+++ xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/main/java/org/xwiki/classloader/internal/DefaultClassLoaderManager.java
@@ -19,15 +19,20 @@
  */
 package org.xwiki.classloader.internal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
 import org.xwiki.classloader.ClassLoaderManager;
 import org.xwiki.classloader.NamespaceURLClassLoader;
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.manager.ComponentLifecycleException;
+import org.xwiki.component.phase.Disposable;
 import org.xwiki.component.phase.Initializable;
 import org.xwiki.component.phase.InitializationException;
 
@@ -39,12 +44,20 @@ import org.xwiki.component.phase.InitializationException;
  */
 @Component
 @Singleton
-public class DefaultClassLoaderManager implements ClassLoaderManager, Initializable
+public class DefaultClassLoaderManager implements ClassLoaderManager, Initializable, Disposable
 {
+    /**
+     * The readonly container loader that should be used as parent of all the namespace classloader.
+     */
+    protected ClassLoader containerClassLoader;
+
     /**
      * The class loader corresponding to null namespace.
      */
-    protected NamespaceURLClassLoader rootClassLoader;
+    private volatile NamespaceURLClassLoader rootClassLoader;
+
+    @Inject
+    private Logger logger;
 
     /**
      * The classloaders stored by namespace.
@@ -55,21 +68,44 @@ public class DefaultClassLoaderManager implements ClassLoaderManager, Initializa
     @Override
     public void initialize() throws InitializationException
     {
-        this.rootClassLoader =
-            new NamespaceURLClassLoader(new URI[] {}, Thread.currentThread().getContextClassLoader(), null);
+        this.containerClassLoader = Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * Allow overriding the system classloader during tests.
+     *
+     * @return a ClassLoader to be used as the system parent
+     */
+    protected ClassLoader getContainerClassLoader()
+    {
+        return this.containerClassLoader;
     }
 
     @Override
     public NamespaceURLClassLoader getURLClassLoader(String namespace, boolean create)
     {
+        // Make sure root classloader exist
+        if (this.rootClassLoader == null) {
+            synchronized (this) {
+                if (this.rootClassLoader == null) {
+                    this.rootClassLoader = new NamespaceURLClassLoader(new URI[] {}, getContainerClassLoader(), null);
+                }
+            }
+        }
+
+        // Return root classloader by default
         NamespaceURLClassLoader wikiClassLoader = this.rootClassLoader;
 
+        // Find classloader corresponding to passed namespace
         if (namespace != null) {
             wikiClassLoader = this.wikiClassLoaderMap.get(namespace);
 
             if (wikiClassLoader == null) {
                 if (create) {
+                    // Create classloader
                     wikiClassLoader = new NamespaceURLClassLoader(new URI[] {}, this.rootClassLoader, namespace);
+
+                    // Store new classloader
                     this.wikiClassLoaderMap.put(namespace, wikiClassLoader);
                 } else {
                     wikiClassLoader = this.rootClassLoader;
@@ -87,14 +123,42 @@ public class DefaultClassLoaderManager implements ClassLoaderManager, Initializa
             for (String namespace : this.wikiClassLoaderMap.keySet()) {
                 dropURLClassLoader(namespace);
             }
+
+            // Reset and close root classloader
+            NamespaceURLClassLoader classloader = this.rootClassLoader;
+            this.rootClassLoader = null;
+            try {
+                classloader.close();
+            } catch (IOException e) {
+                this.logger.warn("Failed to close root Classloader", e);
+            }
         }
     }
 
     @Override
     public void dropURLClassLoader(String namespace)
     {
-        if (this.rootClassLoader != null && namespace != null) {
-            this.wikiClassLoaderMap.remove(namespace);
+        if (namespace == null) {
+            // If root namespace drop all namespaces
+            dropURLClassLoaders();
+        } else {
+            // Remove the classloader from the map
+            NamespaceURLClassLoader classloader = this.wikiClassLoaderMap.remove(namespace);
+
+            if (classloader != null) {
+                // Close the classloader
+                try {
+                    classloader.close();
+                } catch (IOException e) {
+                    this.logger.warn("Failed to close Classloader", e);
+                }
+            }
         }
+    }
+
+    @Override
+    public void dispose() throws ComponentLifecycleException
+    {
+        dropURLClassLoaders();
     }
 }
