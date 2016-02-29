eb2efb6fc7cc8016c651ae6b47be1ed5dd448eb0
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-02-29 15:08:08 +0100

    [misc] Improve codestyle

--- xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/handler/internal/AbstractExtensionHandler.java
+++ xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/handler/internal/AbstractExtensionHandler.java
@@ -71,8 +71,8 @@ public abstract class AbstractExtensionHandler implements ExtensionHandler
     public void upgrade(LocalExtension previousLocalExtension, LocalExtension newLocalExtension, String namespace,
         Request request) throws InstallException
     {
-        upgrade(previousLocalExtension != null ? Arrays.asList(previousLocalExtension) : Collections.EMPTY_LIST,
-            newLocalExtension, namespace, request);
+        upgrade(previousLocalExtension != null ? Arrays.asList((InstalledExtension) previousLocalExtension)
+            : Collections.<InstalledExtension>emptyList(), newLocalExtension, namespace, request);
     }
 
     @Override
