719e348c8479b27c3f66bfbaed737fef6b0005a5
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-08-23 16:46:08 +0200

    XCOMMONS-1053: Make sure to get the event parameter name from the top interface/class
    (cherry picked from commit 2fd0d398c4c5a06e5e0884a951040bb408d43b17)

---
+++ xwiki-commons-core/xwiki-commons-filter/xwiki-commons-filter-api/src/test/java/org/xwiki/filter/internal/ProxyFilterDescriptorManagerTest.java
@@ -0,0 +1,43 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.filter.internal;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.xwiki.filter.test.TestFilter;
+
+public class ProxyFilterDescriptorManagerTest extends AbstractFilterDescriptorManagerTest
+{
+    public ProxyFilterDescriptorManagerTest()
+    {
+        super(Proxy.newProxyInstance(ProxyFilterDescriptorManagerTest.class.getClassLoader(), new Class[] { TestFilter.class },
+            new InvocationHandler()
+            {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+                {
+                    // Do nothing
+                    return null;
+                }
+            }));
+    }
+}
