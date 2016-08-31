0c64061d7589a6308b8905de5d3268a1aff7c648
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-08-31 16:54:03 +0200

    XCOMMONS-1058: HTML cleaner should add rel="noopener" to external links opened in a new window.
    * Fix.

--- xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/filter/LinkFilter.java
+++ xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/filter/LinkFilter.java
@@ -29,7 +29,6 @@ import java.util.Map;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xwiki.component.annotation.Component;
