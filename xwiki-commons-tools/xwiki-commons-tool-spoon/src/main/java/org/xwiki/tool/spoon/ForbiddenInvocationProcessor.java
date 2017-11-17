860b6ce96b8e9ae35e89af74c4edb58d35072bf6
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-11-17 17:59:20 +0100

    [misc] Bulletproofing

--- xwiki-commons-tools/xwiki-commons-tool-spoon/src/main/java/org/xwiki/tool/spoon/ForbiddenInvocationProcessor.java
+++ xwiki-commons-tools/xwiki-commons-tool-spoon/src/main/java/org/xwiki/tool/spoon/ForbiddenInvocationProcessor.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.tool.spoon;
 
 import java.util.Collections;
@@ -11,6 +30,12 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtInvocation;
 
+/**
+ * Failed the build if some code is calling a forbidden method.
+ * 
+ * @version $Id$
+ * @since 9.9RC2
+ */
 public class ForbiddenInvocationProcessor extends AbstractProcessor<CtInvocation<?>>
 {
     // FIXME: replace this by proper configuration when supported by the Spoon Maven plugin, see
@@ -26,7 +51,7 @@ public class ForbiddenInvocationProcessor extends AbstractProcessor<CtInvocation
     {
         CtExpression<?> target = element.getTarget();
 
-        if (target != null) {
+        if (target != null && target.getType() != null) {
             String type = target.getType().getQualifiedName();
             Set<String> methods = METHODS.get(type);
             if (methods != null) {
