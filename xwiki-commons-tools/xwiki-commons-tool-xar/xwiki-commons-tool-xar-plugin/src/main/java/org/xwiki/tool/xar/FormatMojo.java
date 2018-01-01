dff64088b65146d08be0661ef3436d4d03a98e2d
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-01-01 12:33:50 +0100

    XCOMMONS-1326: XAR plugin format goal breaks version in XML declaration
    (cherry picked from commit c3f217be76931cf84379158503d5eb3df3772e4e)

--- xwiki-commons-tools/xwiki-commons-tool-xar/xwiki-commons-tool-xar-plugin/src/main/java/org/xwiki/tool/xar/FormatMojo.java
+++ xwiki-commons-tools/xwiki-commons-tool-xar/xwiki-commons-tool-xar-plugin/src/main/java/org/xwiki/tool/xar/FormatMojo.java
@@ -91,7 +91,6 @@ public class FormatMojo extends AbstractVerifyMojo
         OutputFormat format;
         if (this.pretty) {
             format = new OutputFormat("  ", true, "UTF-8");
-            format.setExpandEmptyElements(false);
         } else {
             format = new OutputFormat(null, false, "UTF-8");
         }
@@ -117,6 +116,8 @@ public class FormatMojo extends AbstractVerifyMojo
 
             if (version.compareTo(version13) >= 0) {
                 writer.writeDeclaration("1.1");
+
+                return ;
             }
         }
 
