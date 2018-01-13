6808dc42f8e6eddf60745682ce97a2eeec2f0ec3
Enigmatrix <enigmatrix2000@gmail.com>
2018-01-13 13:25:20 -0500

    Change usages of DefaultComponentRole<T>'s setRole to setRoleType

--- xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-api/src/main/java/org/xwiki/component/descriptor/DefaultComponentDependency.java
+++ xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-api/src/main/java/org/xwiki/component/descriptor/DefaultComponentDependency.java
@@ -218,7 +218,7 @@ public class DefaultComponentDependency<T> extends DefaultComponentRole<T> imple
 
             setRoleType(new DefaultParameterizedType(ownerType, rawType, role));
         } else {
-            super.setRole(role);
+            super.setRoleType(role);
         }
     }
 }
