546733fe43bef63277b8515da783dec1ad256448
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2018-01-18 17:52:09 +0100

    XCOMMONS-1342: Bad merge when all previous content is replaced in the current version.
    (cherry picked from commit 995008636fcc1c45051e1374f13b23c3ec2a0a7e)

--- xwiki-commons-core/xwiki-commons-diff/xwiki-commons-diff-api/src/main/java/org/xwiki/diff/internal/DefaultDiffManager.java
+++ xwiki-commons-core/xwiki-commons-diff/xwiki-commons-diff-api/src/main/java/org/xwiki/diff/internal/DefaultDiffManager.java
@@ -132,7 +132,7 @@ public class DefaultDiffManager implements DiffManager
             } else if (isFullyModified(commonAncestor, patchCurrent)) {
                 // If current is completely modified compared to the common ancestor we assume any change in next is
                 // a conflict
-                // ... except if the current content is similar to the next one!
+                // ... except if the current content is identical to the next one!
                 if (!current.equals(next)) {
                     Delta<E> deltaNext = nextElement(patchNext);
                     Delta<E> deltaCurrent = nextElement(patchCurrent);
