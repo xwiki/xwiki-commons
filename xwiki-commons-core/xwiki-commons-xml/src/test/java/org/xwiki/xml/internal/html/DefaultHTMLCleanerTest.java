5701cd497d360dacd5b9a358cc8248a302d204a7
Vincent Massol <vincent@massol.net>
2018-04-15 09:35:24 +0200

    [Misc] Move some tests to JUnit5

--- xwiki-commons-core/xwiki-commons-xml/src/test/java/org/xwiki/xml/internal/html/DefaultHTMLCleanerTest.java
+++ xwiki-commons-core/xwiki-commons-xml/src/test/java/org/xwiki/xml/internal/html/DefaultHTMLCleanerTest.java
@@ -26,14 +26,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.test.annotation.ComponentList;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
-import org.xwiki.xml.html.HTMLCleaner;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.xml.html.HTMLCleanerConfiguration;
 import org.xwiki.xml.html.HTMLUtils;
 import org.xwiki.xml.html.filter.HTMLFilter;
@@ -45,14 +44,25 @@ import org.xwiki.xml.internal.html.filter.ListFilter;
 import org.xwiki.xml.internal.html.filter.ListItemFilter;
 import org.xwiki.xml.internal.html.filter.UniqueIdFilter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Unit tests for {@link org.xwiki.xml.internal.html.DefaultHTMLCleaner}.
  *
  * @version $Id$
  * @since 1.6M1
  */
-@ComponentList({ ListFilter.class, ListItemFilter.class, FontFilter.class, BodyFilter.class, AttributeFilter.class,
-UniqueIdFilter.class, DefaultHTMLCleaner.class, LinkFilter.class })
+@ComponentTest
+@ComponentList({
+    ListFilter.class,
+    ListItemFilter.class,
+    FontFilter.class,
+    BodyFilter.class,
+    AttributeFilter.class,
+    UniqueIdFilter.class,
+    DefaultHTMLCleaner.class,
+    LinkFilter.class
+})
 public class DefaultHTMLCleanerTest
 {
     public static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
@@ -63,9 +73,8 @@ public class DefaultHTMLCleanerTest
 
     private static final String FOOTER = "</body></html>\n";
 
-    @Rule
-    public final MockitoComponentMockingRule<HTMLCleaner> mocker = new MockitoComponentMockingRule<HTMLCleaner>(
-        DefaultHTMLCleaner.class);
+    @InjectMockComponents
+    private DefaultHTMLCleaner cleaner;
 
     @Test
     public void elementExpansion() throws ComponentLookupException
@@ -233,13 +242,12 @@ public class DefaultHTMLCleanerTest
     @Test
     public void explicitFilterList() throws ComponentLookupException
     {
-        HTMLCleanerConfiguration configuration = this.mocker.getComponentUnderTest().getDefaultConfiguration();
-        configuration.setFilters(Collections.<HTMLFilter>emptyList());
-        String result =
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader("something"), configuration));
+        HTMLCleanerConfiguration configuration = this.cleaner.getDefaultConfiguration();
+        configuration.setFilters(Collections.emptyList());
+        String result = HTMLUtils.toString(this.cleaner.clean(new StringReader("something"), configuration));
         // Note that if the default Body filter had been executed the result would have been:
         // <p>something</p>.
-        Assert.assertEquals(HEADER_FULL + "something" + FOOTER, result);
+        assertEquals(HEADER_FULL + "something" + FOOTER, result);
     }
 
     /**
@@ -248,22 +256,19 @@ public class DefaultHTMLCleanerTest
     @Test
     public void restrictedHtml() throws ComponentLookupException
     {
-        HTMLCleanerConfiguration configuration = this.mocker.getComponentUnderTest().getDefaultConfiguration();
+        HTMLCleanerConfiguration configuration = this.cleaner.getDefaultConfiguration();
         Map<String, String> parameters = new HashMap<String, String>();
         parameters.putAll(configuration.getParameters());
         parameters.put("restricted", "true");
         configuration.setParameters(parameters);
 
-        String result =
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(
-                new StringReader("<script>alert(\"foo\")</script>"), configuration));
-        Assert.assertEquals(HEADER_FULL + "<pre>alert(\"foo\")</pre>" + FOOTER, result);
+        String result = HTMLUtils.toString(this.cleaner.clean(
+            new StringReader("<script>alert(\"foo\")</script>"), configuration));
+        assertEquals(HEADER_FULL + "<pre>alert(\"foo\")</pre>" + FOOTER, result);
 
-        result =
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(
-                new StringReader("<style>p {color:white;}</style>"), configuration));
-        Assert.assertEquals(HEADER_FULL + "<pre>p {color:white;}</pre>" + FOOTER, result);
-
+        result = HTMLUtils.toString(this.cleaner.clean(
+            new StringReader("<style>p {color:white;}</style>"), configuration));
+        assertEquals(HEADER_FULL + "<pre>p {color:white;}</pre>" + FOOTER, result);
     }
 
     /**
@@ -279,16 +284,16 @@ public class DefaultHTMLCleanerTest
      * Test {@link UniqueIdFilter}.
      */
     @Test
-    public void duplicateIds() throws Exception
+    public void duplicateIds(ComponentManager componentManager) throws Exception
     {
         String actual = "<p id=\"x\">1</p><p id=\"xy\">2</p><p id=\"x\">3</p>";
         String expected = "<p id=\"x\">1</p><p id=\"xy\">2</p><p id=\"x0\">3</p>";
-        HTMLCleanerConfiguration config = this.mocker.getComponentUnderTest().getDefaultConfiguration();
-        List<HTMLFilter> filters = new ArrayList<HTMLFilter>(config.getFilters());
-        filters.add(this.mocker.<HTMLFilter>getInstance(HTMLFilter.class, "uniqueId"));
+        HTMLCleanerConfiguration config = this.cleaner.getDefaultConfiguration();
+        List<HTMLFilter> filters = new ArrayList<>(config.getFilters());
+        filters.add(componentManager.getInstance(HTMLFilter.class, "uniqueId"));
         config.setFilters(filters);
-        Assert.assertEquals(HEADER_FULL + expected + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(actual), config)));
+        assertEquals(HEADER_FULL + expected + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(actual), config)));
     }
 
     /**
@@ -311,7 +316,7 @@ public class DefaultHTMLCleanerTest
      * <a href="https://jira.xwiki.org/browse/XWIKI-9753">XWIKI-9753</a>).
      */
     @Test
-    @Ignore("See https://jira.xwiki.org/browse/XWIKI-9753")
+    @Disabled("See https://jira.xwiki.org/browse/XWIKI-9753")
     public void cleanTitleWithNamespace() throws Exception
     {
         // Test with TITLE in HEAD
@@ -327,8 +332,8 @@ public class DefaultHTMLCleanerTest
                 + "        <title>SVG Title Demo example</title>\n"
                 + "        <rect height=\"50\" style=\"fill:none; stroke:blue; stroke-width:1px\" width=\"200\" x=\"10\" "
                 + "y=\"10\"></rect>\n" + "      </g>\n" + "    </svg>\n" + "    <p>after</p>\n";
-        Assert.assertEquals(HEADER + input + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(input))));
+        assertEquals(HEADER + input + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(input))));
     }
 
     /**
@@ -336,20 +341,20 @@ public class DefaultHTMLCleanerTest
      * {@link HTMLCleanerConfiguration#NAMESPACES_AWARE} is set to false.
      */
     @Test
-    @Ignore("See https://sourceforge.net/p/htmlcleaner/bugs/168/")
+    @Disabled("See https://sourceforge.net/p/htmlcleaner/bugs/168/")
     public void cleanHTMLTagWithNamespace() throws Exception
     {
         String input = "<html xmlns=\"http://www.w3.org/1999/xhtml\"><head></head><body>";
 
         // Default
-        Assert.assertEquals(HEADER + input + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(input))));
+        assertEquals(HEADER + input + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(input))));
 
         // Configured for namespace awareness being false
-        HTMLCleanerConfiguration config = this.mocker.getComponentUnderTest().getDefaultConfiguration();
+        HTMLCleanerConfiguration config = this.cleaner.getDefaultConfiguration();
         config.setParameters(Collections.singletonMap(HTMLCleanerConfiguration.NAMESPACES_AWARE, "false"));
-        Assert.assertEquals(HEADER + "<html><head></head><body>" + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(input), config)));
+        assertEquals(HEADER + "<html><head></head><body>" + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(input), config)));
     }
 
     /**
@@ -429,13 +434,13 @@ public class DefaultHTMLCleanerTest
 
     private void assertHTML(String expected, String actual) throws ComponentLookupException
     {
-        Assert.assertEquals(HEADER_FULL + expected + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(actual))));
+        assertEquals(HEADER_FULL + expected + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(actual))));
     }
 
     private void assertHTMLWithHeadContent(String expected, String actual) throws ComponentLookupException
     {
-        Assert.assertEquals(HEADER + "<html><head>" + expected + "</head><body>" + FOOTER,
-            HTMLUtils.toString(this.mocker.getComponentUnderTest().clean(new StringReader(actual))));
+        assertEquals(HEADER + "<html><head>" + expected + "</head><body>" + FOOTER,
+            HTMLUtils.toString(this.cleaner.clean(new StringReader(actual))));
     }
 }
