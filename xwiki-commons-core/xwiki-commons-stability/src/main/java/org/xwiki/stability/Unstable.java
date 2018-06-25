e8ca631240fe07b4c7ba9f94853c386772986e5c
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-06-25 17:40:35 +0200

    [misc] Allow applying @Unstable annotation on enum fields

--- xwiki-commons-core/xwiki-commons-stability/src/main/java/org/xwiki/stability/Unstable.java
+++ xwiki-commons-core/xwiki-commons-stability/src/main/java/org/xwiki/stability/Unstable.java
@@ -24,6 +24,7 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -37,7 +38,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  */
 @Documented
 @Retention(RUNTIME)
-@Target({ TYPE, METHOD })
+@Target({ TYPE, METHOD, FIELD })
 @Inherited
 public @interface Unstable
 {
