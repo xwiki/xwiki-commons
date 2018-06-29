a53199e0b66f1f14c04fb0f6288d01187cd843c3
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-06-29 12:14:52 +0200

    [misc] Allow putting `@Unstable` on a constructor

--- xwiki-commons-core/xwiki-commons-stability/src/main/java/org/xwiki/stability/Unstable.java
+++ xwiki-commons-core/xwiki-commons-stability/src/main/java/org/xwiki/stability/Unstable.java
@@ -24,6 +24,7 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
@@ -38,7 +39,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  */
 @Documented
 @Retention(RUNTIME)
-@Target({ TYPE, METHOD, FIELD })
+@Target({ TYPE, METHOD, FIELD, CONSTRUCTOR })
 @Inherited
 public @interface Unstable
 {
