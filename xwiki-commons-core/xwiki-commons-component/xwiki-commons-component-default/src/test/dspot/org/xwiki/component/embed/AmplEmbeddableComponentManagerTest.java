f78d89d0f49c2f25c36f2b1444dda49990149b67
Vincent Massol <vincent@massol.net>
2018-09-04 15:22:22 +0200

    [Misc] Apply XWiki code style

--- xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default/src/test/dspot/org/xwiki/component/embed/AmplEmbeddableComponentManagerTest.java
+++ xwiki-commons-core/xwiki-commons-component/xwiki-commons-component-default/src/test/dspot/org/xwiki/component/embed/AmplEmbeddableComponentManagerTest.java
@@ -19,7 +19,6 @@
  */
 package org.xwiki.component.embed;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -34,73 +33,92 @@ import org.xwiki.component.phase.Disposable;
 import org.xwiki.component.phase.Initializable;
 import org.xwiki.component.phase.InitializationException;
 
+public class AmplEmbeddableComponentManagerTest
+{
+    public static interface Role
+    {
+    }
 
-public class AmplEmbeddableComponentManagerTest {
-    public static interface Role {}
+    public static class RoleImpl implements AmplEmbeddableComponentManagerTest.Role
+    {
+    }
 
-    public static class RoleImpl implements AmplEmbeddableComponentManagerTest.Role {}
-
-    public static class OtherRoleImpl implements AmplEmbeddableComponentManagerTest.Role {}
+    public static class OtherRoleImpl implements AmplEmbeddableComponentManagerTest.Role
+    {
+    }
 
     private static String lastDisposedComponent;
 
-    public static class InitializableRoleImpl implements AmplEmbeddableComponentManagerTest.Role , Initializable {
+    public static class InitializableRoleImpl implements AmplEmbeddableComponentManagerTest.Role, Initializable
+    {
         private boolean initialized = false;
 
         @Override
-        public void initialize() throws InitializationException {
+        public void initialize() throws InitializationException
+        {
             this.initialized = true;
         }
 
-        public boolean isInitialized() {
+        public boolean isInitialized()
+        {
             return this.initialized;
         }
     }
 
-    public static class DisposableRoleImpl implements AmplEmbeddableComponentManagerTest.Role , Disposable {
+    public static class DisposableRoleImpl implements AmplEmbeddableComponentManagerTest.Role, Disposable
+    {
         private boolean finalized = false;
 
         @Override
-        public void dispose() throws ComponentLifecycleException {
+        public void dispose() throws ComponentLifecycleException
+        {
             this.finalized = true;
             AmplEmbeddableComponentManagerTest.lastDisposedComponent = "DisposableRoleImpl";
         }
 
-        public boolean isFinalized() {
+        public boolean isFinalized()
+        {
             return this.finalized;
         }
     }
 
     @DisposePriority(2000)
-    public static class DisposableWithPriorityRoleImpl implements AmplEmbeddableComponentManagerTest.Role , Disposable {
+    public static class DisposableWithPriorityRoleImpl implements AmplEmbeddableComponentManagerTest.Role, Disposable
+    {
         private boolean finalized = false;
 
         @Override
-        public void dispose() throws ComponentLifecycleException {
+        public void dispose() throws ComponentLifecycleException
+        {
             this.finalized = true;
             AmplEmbeddableComponentManagerTest.lastDisposedComponent = "DisposableWithPriorityRoleImpl";
         }
 
-        public boolean isFinalized() {
+        public boolean isFinalized()
+        {
             return this.finalized;
         }
     }
 
-    public static class LoggingRoleImpl implements AmplEmbeddableComponentManagerTest.Role {
+    public static class LoggingRoleImpl implements AmplEmbeddableComponentManagerTest.Role
+    {
         private Logger logger;
 
-        public Logger getLogger() {
+        public Logger getLogger()
+        {
             return this.logger;
         }
     }
 
     @Test(timeout = 10000)
-    public void testHasComponent() throws Exception {
+    public void testHasComponent() throws Exception
+    {
         EmbeddableComponentManager ecm = new EmbeddableComponentManager();
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getParent());
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getComponentEventManager());
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getNamespace());
-        DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> d1 = new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
+        DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> d1 =
+            new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
         d1.setRoleType(AmplEmbeddableComponentManagerTest.Role.class);
         d1.setRoleHint("default");
         ecm.registerComponent(d1);
@@ -110,13 +128,16 @@ public class AmplEmbeddableComponentManagerTest {
         Assert.assertTrue(o_testHasComponent__9);
     }
 
-    private ComponentManager createParentComponentManager() throws Exception {
+    private ComponentManager createParentComponentManager() throws Exception
+    {
         return createParentComponentManager(null);
     }
 
-    private ComponentManager createParentComponentManager(String hint) throws Exception {
+    private ComponentManager createParentComponentManager(String hint) throws Exception
+    {
         EmbeddableComponentManager parent = new EmbeddableComponentManager();
-        DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> cd = new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
+        DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> cd =
+            new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
         cd.setRoleType(AmplEmbeddableComponentManagerTest.Role.class);
         cd.setImplementation(AmplEmbeddableComponentManagerTest.RoleImpl.class);
         if (hint != null) {
@@ -127,12 +148,14 @@ public class AmplEmbeddableComponentManagerTest {
     }
 
     @Test(timeout = 10000)
-    public void testRelease() throws Exception {
+    public void testRelease() throws Exception
+    {
         final EmbeddableComponentManager ecm = new EmbeddableComponentManager();
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getParent());
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getComponentEventManager());
         Assert.assertNull(((EmbeddableComponentManager) (ecm)).getNamespace());
-        final DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> cd = new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
+        final DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role> cd =
+            new DefaultComponentDescriptor<AmplEmbeddableComponentManagerTest.Role>();
         cd.setRoleType(AmplEmbeddableComponentManagerTest.Role.class);
         cd.setImplementation(AmplEmbeddableComponentManagerTest.RoleImpl.class);
         AmplEmbeddableComponentManagerTest.Role roleImpl = new AmplEmbeddableComponentManagerTest.RoleImpl();
@@ -146,10 +169,12 @@ public class AmplEmbeddableComponentManagerTest {
         ecm.getInstance(AmplEmbeddableComponentManagerTest.Role.class);
     }
 
-    public static class ComponentDescriptorRoleImpl implements AmplEmbeddableComponentManagerTest.Role {
+    public static class ComponentDescriptorRoleImpl implements AmplEmbeddableComponentManagerTest.Role
+    {
         private ComponentDescriptor<AmplEmbeddableComponentManagerTest.ComponentDescriptorRoleImpl> descriptor;
 
-        public ComponentDescriptor<AmplEmbeddableComponentManagerTest.ComponentDescriptorRoleImpl> getComponentDescriptor() {
+        public ComponentDescriptor<AmplEmbeddableComponentManagerTest.ComponentDescriptorRoleImpl> getComponentDescriptor()
+        {
             return this.descriptor;
         }
     }
