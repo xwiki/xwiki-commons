134ac7932a7edcaa0094ae26347362165ad4cba7
Vincent Massol <vincent@massol.net>
2019-08-02 16:06:26 +0200

    [Misc] Convert to JUnit5

--- xwiki-commons-core/xwiki-commons-crypto/xwiki-commons-crypto-pkix/src/test/java/org/xwiki/crypto/pkix/internal/BcX509CertificateFactoryTest.java
+++ xwiki-commons-core/xwiki-commons-crypto/xwiki-commons-crypto-pkix/src/test/java/org/xwiki/crypto/pkix/internal/BcX509CertificateFactoryTest.java
@@ -21,16 +21,14 @@ package org.xwiki.crypto.pkix.internal;
 
 import java.util.EnumSet;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.xwiki.crypto.AbstractPKIXTest;
 import org.xwiki.crypto.BinaryStringEncoder;
 import org.xwiki.crypto.internal.asymmetric.keyfactory.BcDSAKeyFactory;
 import org.xwiki.crypto.internal.asymmetric.keyfactory.BcRSAKeyFactory;
 import org.xwiki.crypto.internal.digest.factory.BcSHA1DigestFactory;
 import org.xwiki.crypto.internal.encoder.Base64BinaryStringEncoder;
-import org.xwiki.crypto.pkix.CertificateFactory;
 import org.xwiki.crypto.pkix.params.CertifiedPublicKey;
 import org.xwiki.crypto.pkix.params.x509certificate.X509CertifiedPublicKey;
 import org.xwiki.crypto.pkix.params.x509certificate.extension.ExtendedKeyUsages;
@@ -40,36 +38,46 @@ import org.xwiki.crypto.signer.internal.factory.BcDSAwithSHA1SignerFactory;
 import org.xwiki.crypto.signer.internal.factory.BcSHA1withRsaSignerFactory;
 import org.xwiki.crypto.signer.internal.factory.DefaultSignerFactory;
 import org.xwiki.test.annotation.ComponentList;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectComponentManager;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
+import org.xwiki.test.mockito.MockitoComponentManager;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ComponentList({Base64BinaryStringEncoder.class, BcRSAKeyFactory.class, BcDSAKeyFactory.class,
+@ComponentTest
+@ComponentList({ Base64BinaryStringEncoder.class, BcRSAKeyFactory.class, BcDSAKeyFactory.class,
     BcSHA1DigestFactory.class, BcSHA1withRsaSignerFactory.class,
-    DefaultSignerFactory.class, BcDSAwithSHA1SignerFactory.class})
+    DefaultSignerFactory.class, BcDSAwithSHA1SignerFactory.class })
 public class BcX509CertificateFactoryTest extends AbstractPKIXTest
 {
-    @Rule
-    public final MockitoComponentMockingRule<CertificateFactory> mocker =
-        new MockitoComponentMockingRule<CertificateFactory>(BcX509CertificateFactory.class);
+    @InjectMockComponents
+    private BcX509CertificateFactory factory;
 
-    private CertificateFactory factory;
+    @InjectComponentManager
+    private MockitoComponentManager componentManager;
+
     private static byte[] v1CaCert;
+
     private static byte[] v1Cert;
+
     private static byte[] v3CaCert;
+
     private static byte[] v3InterCaCert;
+
     private static byte[] v3Cert;
 
-    public void setupTest(MockitoComponentMockingRule<CertificateFactory> mocker) throws Exception
+    @BeforeEach
+    public void setupTest() throws Exception
     {
         // Decode keys once for all tests.
         if (v1CaCert == null) {
-            BinaryStringEncoder base64encoder = mocker.getInstance(BinaryStringEncoder.class, "Base64");
+            BinaryStringEncoder base64encoder = componentManager.getInstance(BinaryStringEncoder.class, "Base64");
             v1CaCert = base64encoder.decode(V1_CA_CERT);
             v1Cert = base64encoder.decode(V1_CERT);
             v3CaCert = base64encoder.decode(V3_CA_CERT);
@@ -78,19 +86,12 @@ public class BcX509CertificateFactoryTest extends AbstractPKIXTest
         }
     }
 
-    @Before
-    public void configure() throws Exception
-    {
-        factory = mocker.getComponentUnderTest();
-        setupTest(mocker);
-    }
-
     @Test
-    public void testV1CaCert() throws Exception
+    public void v1CaCert() throws Exception
     {
         CertifiedPublicKey certificate = factory.decode(v1CaCert);
 
-        assertTrue("CA should verify itself.", certificate.isSignedBy(certificate.getPublicKeyParameters()));
+        assertTrue(certificate.isSignedBy(certificate.getPublicKeyParameters()), "CA should verify itself.");
 
         assertThat(certificate, instanceOf(X509CertifiedPublicKey.class));
         X509CertifiedPublicKey cert = (X509CertifiedPublicKey) certificate;
@@ -99,12 +100,13 @@ public class BcX509CertificateFactoryTest extends AbstractPKIXTest
     }
 
     @Test
-    public void testV1Cert() throws Exception
+    public void v1Cert() throws Exception
     {
         CertifiedPublicKey caCert = factory.decode(v1CaCert);
         CertifiedPublicKey certificate = factory.decode(v1Cert);
 
-        assertTrue("End certificate should be verified by CA.", certificate.isSignedBy(caCert.getPublicKeyParameters()));
+        assertTrue(certificate.isSignedBy(caCert.getPublicKeyParameters()),
+            "End certificate should be verified by CA.");
 
         assertThat(certificate, instanceOf(X509CertifiedPublicKey.class));
         X509CertifiedPublicKey cert = (X509CertifiedPublicKey) certificate;
@@ -112,19 +114,22 @@ public class BcX509CertificateFactoryTest extends AbstractPKIXTest
     }
 
     @Test
-    public void testV3CaCert() throws Exception
+    public void v3CaCert() throws Exception
     {
         CertifiedPublicKey certificate = factory.decode(v3CaCert);
 
-        assertTrue("CA should verify itself.", certificate.isSignedBy(certificate.getPublicKeyParameters()));
+        assertTrue(certificate.isSignedBy(certificate.getPublicKeyParameters()), "CA should verify itself.");
 
         assertThat(certificate, instanceOf(X509CertifiedPublicKey.class));
         X509CertifiedPublicKey cert = (X509CertifiedPublicKey) certificate;
         assertThat(cert.getVersionNumber(), equalTo(3));
 
-        assertTrue("Basic constraints should be critical.", cert.getExtensions().isCritical(X509Extensions.BASIC_CONSTRAINTS_OID));
-        assertTrue("Basic constraints should be set to CA.", cert.getExtensions().hasCertificateAuthorityBasicConstraints());
-        assertTrue("KeyUsage extension should be critical.", cert.getExtensions().isCritical(KeyUsage.OID));
+        assertTrue(cert.getExtensions().isCritical(X509Extensions.BASIC_CONSTRAINTS_OID),
+            "Basic constraints should be critical.");
+        assertTrue(cert.getExtensions().hasCertificateAuthorityBasicConstraints(),
+            "Basic constraints should be set to CA.");
+        assertTrue(cert.getExtensions().isCritical(KeyUsage.OID),
+            "KeyUsage extension should be critical.");
         assertThat(cert.getExtensions().getKeyUsage(), equalTo(EnumSet.of(KeyUsage.keyCertSign,
             KeyUsage.cRLSign)));
         assertThat(cert.getAuthorityKeyIdentifier(), notNullValue());
@@ -134,24 +139,24 @@ public class BcX509CertificateFactoryTest extends AbstractPKIXTest
     }
 
     @Test
-    public void testV3InterCACert() throws Exception
+    public void v3InterCACert() throws Exception
     {
         CertifiedPublicKey caCert = factory.decode(v3CaCert);
         CertifiedPublicKey interCaCert = factory.decode(v3InterCaCert);
 
-        assertTrue("Intermediate CA certificate should be verified by CA.",
-            interCaCert.isSignedBy(caCert.getPublicKeyParameters()));
+        assertTrue(interCaCert.isSignedBy(caCert.getPublicKeyParameters()),
+            "Intermediate CA certificate should be verified by CA.");
 
         assertThat(interCaCert, instanceOf(X509CertifiedPublicKey.class));
         X509CertifiedPublicKey cert = (X509CertifiedPublicKey) interCaCert;
         assertThat(cert.getVersionNumber(), equalTo(3));
 
-        assertTrue("Basic constraints should be critical.",
-            cert.getExtensions().isCritical(X509Extensions.BASIC_CONSTRAINTS_OID));
-        assertTrue("Basic constraints should be set to CA.",
-            cert.getExtensions().hasCertificateAuthorityBasicConstraints());
+        assertTrue(cert.getExtensions().isCritical(X509Extensions.BASIC_CONSTRAINTS_OID),
+            "Basic constraints should be critical.");
+        assertTrue(cert.getExtensions().hasCertificateAuthorityBasicConstraints(),
+            "Basic constraints should be set to CA.");
         assertThat(cert.getExtensions().getBasicConstraintsPathLen(), equalTo(0));
-        assertTrue("KeyUsage extension should be critical.", cert.getExtensions().isCritical(KeyUsage.OID));
+        assertTrue(cert.getExtensions().isCritical(KeyUsage.OID), "KeyUsage extension should be critical.");
         assertThat(cert.getExtensions().getKeyUsage(), equalTo(EnumSet.of(KeyUsage.keyCertSign,
             KeyUsage.cRLSign)));
 
@@ -161,31 +166,30 @@ public class BcX509CertificateFactoryTest extends AbstractPKIXTest
     }
 
     @Test
-    public void testV3Cert() throws Exception
+    public void v3Cert() throws Exception
     {
         CertifiedPublicKey interCaCert = factory.decode(v3InterCaCert);
         CertifiedPublicKey certificate = factory.decode(v3Cert);
 
-        assertTrue("End certificate should be verified by CA.",
-            certificate.isSignedBy(interCaCert.getPublicKeyParameters()));
+        assertTrue(certificate.isSignedBy(interCaCert.getPublicKeyParameters()),
+            "End certificate should be verified by CA.");
 
         assertThat(certificate, instanceOf(X509CertifiedPublicKey.class));
         X509CertifiedPublicKey cert = (X509CertifiedPublicKey) certificate;
         assertThat(cert.getVersionNumber(), equalTo(3));
 
-        assertTrue("KeyUsage extension should be critical.", cert.getExtensions().isCritical(KeyUsage.OID));
+        assertTrue(cert.getExtensions().isCritical(KeyUsage.OID), "KeyUsage extension should be critical.");
         assertThat(cert.getExtensions().getKeyUsage(), equalTo(EnumSet.of(KeyUsage.digitalSignature,
             KeyUsage.dataEncipherment)));
-        assertFalse("ExtendedKeyUsage extension should be non critical.",
-            cert.getExtensions().isCritical(ExtendedKeyUsages.OID));
+        assertFalse(cert.getExtensions().isCritical(ExtendedKeyUsages.OID),
+            "ExtendedKeyUsage extension should be non critical.");
         assertThat(cert.getExtensions().getExtendedKeyUsage().getAll().toArray(new String[0]), equalTo(
-            new String[]{ExtendedKeyUsages.EMAIL_PROTECTION}));
-        assertTrue("Email data protection extended usage should be set.",
-            cert.getExtensions().getExtendedKeyUsage().hasUsage(ExtendedKeyUsages.EMAIL_PROTECTION));
+            new String[]{ ExtendedKeyUsages.EMAIL_PROTECTION }));
+        assertTrue(cert.getExtensions().getExtendedKeyUsage().hasUsage(ExtendedKeyUsages.EMAIL_PROTECTION),
+            "Email data protection extended usage should be set.");
 
         assertThat(cert.getAuthorityKeyIdentifier(),
             equalTo(((X509CertifiedPublicKey) interCaCert).getSubjectKeyIdentifier()));
         assertThat(cert.isRootCA(), equalTo(false));
     }
-
 }
