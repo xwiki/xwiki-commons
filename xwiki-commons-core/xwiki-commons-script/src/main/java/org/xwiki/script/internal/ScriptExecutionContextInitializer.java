bdb5e1dc3300a9e37ce5ec4e4371875f192a8fa7
Thomas Mortagne <thomas.mortagne@gmail.com>
2019-09-04 12:11:07 +0200

    XCOMMONS-1734: The script context stored in the ExecutionContext is not inherited

--- xwiki-commons-core/xwiki-commons-script/src/main/java/org/xwiki/script/internal/ScriptExecutionContextInitializer.java
+++ xwiki-commons-core/xwiki-commons-script/src/main/java/org/xwiki/script/internal/ScriptExecutionContextInitializer.java
@@ -47,6 +47,14 @@ public class ScriptExecutionContextInitializer implements ExecutionContextInitia
     @Override
     public void initialize(ExecutionContext executionContext) throws ExecutionContextException
     {
+        if (!executionContext.hasProperty(SCRIPT_CONTEXT_ID)) {
+            executionContext.newProperty(SCRIPT_CONTEXT_ID)
+                .cloneValue()
+                .inherited()
+                .initial(new SimpleScriptContext())
+                .declare();
+        }
+
         // We're storing an instance of the Script Context class in the Execution Context so that it can be
         // shared between different script invocations during the lifetime of the Execution Context.
         executionContext.setProperty(SCRIPT_CONTEXT_ID, new SimpleScriptContext());
