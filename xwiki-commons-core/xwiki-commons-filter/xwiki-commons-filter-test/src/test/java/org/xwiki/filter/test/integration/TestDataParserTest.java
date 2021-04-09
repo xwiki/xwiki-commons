ce1731f97d6749d2af89b60a0a165e7c86def38d
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-04-09 16:58:53 +0200

    XCOMMONS-2179: Impossible to pass configuration value containing "=" in a filter test
    (cherry picked from commit 767cbb70a2ceece6f7b678d412f1bbcd2614e97d)

--- xwiki-commons-core/xwiki-commons-filter/xwiki-commons-filter-test/src/test/java/org/xwiki/filter/test/integration/TestDataParserTest.java
+++ xwiki-commons-core/xwiki-commons-filter/xwiki-commons-filter-test/src/test/java/org/xwiki/filter/test/integration/TestDataParserTest.java
@@ -19,16 +19,23 @@
  */
 package org.xwiki.filter.test.integration;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit tests for {@link TestDataParser}.
  *
  * @version $Id$
  */
-public class TestDataParserTest
+class TestDataParserTest
 {
     @Test
     void interpret()
@@ -39,5 +46,33 @@ public class TestDataParserTest
         assertEquals("value", TestDataParser.interpret("${{{key}}}"));
         assertEquals("testvaluetest", TestDataParser.interpret("test${{{key}}}test"));
         assertEquals("test${{{key}}}", TestDataParser.interpret("test\\${{{key}}}"));
+    }
+
+    private Map<String, String> toMap(String... elements)
+    {
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < elements.length; i += 2) {
+            map.put(elements[i], elements[i + 1]);
+        }
+
+        return map;
+    }
+
+    @Test
+    void parse() throws IOException
+    {
+        TestDataParser parser = new TestDataParser();
+
+        TestResourceData data = parser.parse(IOUtils.toInputStream("", StandardCharsets.UTF_8), "resourceName");
+        assertEquals("resourceName", data.resourceName);
+        assertTrue(data.configuration.isEmpty());
+
+        assertEquals(toMap("key", "value"),
+            parser.parse(IOUtils.toInputStream(".configuration.key=value", StandardCharsets.UTF_8),
+                "resourceName").configuration);
+        assertEquals(toMap("key", "key=value"),
+            parser.parse(IOUtils.toInputStream(".configuration.key=key=value", StandardCharsets.UTF_8),
+                "resourceName").configuration);
     }
 }
