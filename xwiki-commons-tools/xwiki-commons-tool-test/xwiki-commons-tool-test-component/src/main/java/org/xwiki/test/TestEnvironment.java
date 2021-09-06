c6d9495dc970d19746c3ec0c4ff31a9ab34c69ce
Manuel Leduc <manuel.leduc@xwiki.com>
2021-09-06 10:51:36 +0200

    [Misc] Update TestEnvironment#getResource to only resolve resources locale to the tested module

--- xwiki-commons-tools/xwiki-commons-tool-test/xwiki-commons-tool-test-component/src/main/java/org/xwiki/test/TestEnvironment.java
+++ xwiki-commons-tools/xwiki-commons-tool-test/xwiki-commons-tool-test-component/src/main/java/org/xwiki/test/TestEnvironment.java
@@ -21,10 +21,20 @@ package org.xwiki.test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.environment.Environment;
+
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
 
 /**
  * Environment implementation for tests that looks for resource in the classloader used to load this class. The
@@ -39,6 +49,9 @@ public class TestEnvironment implements Environment
     private File temporaryDirectory = XWikiTempDirUtil.createTemporaryDirectory();
 
     private File permanentDirectory = XWikiTempDirUtil.createTemporaryDirectory();
+
+    @Inject
+    private Logger logger;
 
     @Override
     public File getTemporaryDirectory()
@@ -55,7 +68,35 @@ public class TestEnvironment implements Environment
     @Override
     public URL getResource(String resourceName)
     {
-        return getClass().getResource(resourceName);
+        try {
+            // Get the test-classes directory.
+            URL resource = getClass().getResource(File.pathSeparator);
+            if (resource == null) {
+                return null;
+            }
+            // Resolve the classes directory relatively to the test-classes directory.
+            Path classesDirectory = Paths.get(resource.toURI())
+                .getParent()
+                .resolve("classes");
+            // Replace the absolute path the relative one to be able to resolve it relatively to the classes directory.
+            String cleanResourceName = resourceName;
+            if (resourceName.startsWith(File.pathSeparator)) {
+                cleanResourceName = resourceName.substring(1);
+            }
+            URI resourceURI = classesDirectory
+                .resolve(cleanResourceName)
+                .toUri();
+            // Checks if the resolved URI correspond to a resource in the test directory.
+            if (Paths.get(resourceURI).toFile().exists()) {
+                return resourceURI.toURL();
+            } else {
+                return null;
+            }
+        } catch (URISyntaxException | MalformedURLException e) {
+            this.logger.warn("Failed to resolve the resourceName [{}]. Cause: [{}]", resourceName,
+                getRootCauseMessage(e));
+        }
+        return null;
     }
 
     @Override
