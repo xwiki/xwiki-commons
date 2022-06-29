b2bfab6f2fedbe402be8183511d24a062fbe300d
Thomas Mortagne <thomas.mortagne@gmail.com>
2022-06-29 16:37:25 +0200

    XWIKI-19903: Introduce an enforcer to forbidd specific type of dependencies * add support for more conditions
    (cherry picked from commit c8676cffec69d4e4ab29e21c9f7549f8f4b90ea0)

--- xwiki-commons-core/xwiki-commons-blame/xwiki-commons-blame-api/src/test/java/org/xwiki/blame/internal/DefaultBlameManagerTest.java
+++ xwiki-commons-core/xwiki-commons-blame/xwiki-commons-blame-api/src/test/java/org/xwiki/blame/internal/DefaultBlameManagerTest.java
@@ -36,7 +36,9 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ComponentTest
 public class DefaultBlameManagerTest
