b9c7ac84908f3b1b69c2c341be00a53dcd3aa7e6
Vincent Massol <vincent@massol.net>
2022-11-09 15:15:25 +0100

    XCOMMONS-2561: Use Carte's xml-dtd EntityResolver * Add unit test * Fix typo * Increase test coverage threshold

---
+++ xwiki-commons-core/xwiki-commons-xml/src/test/java/org/xwiki/xml/internal/html/LocalEntityResolverTest.java
@@ -0,0 +1,64 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.xml.internal.html;
+
+import java.io.ByteArrayInputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.junit.jupiter.api.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xwiki.xml.internal.LocalEntityResolver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Unit tests for {@link LocalEntityResolver}.
+ *
+ * @version $Id$
+ */
+public class LocalEntityResolverTest
+{
+    /**
+     * Verify we're not subject to XXE attacks and that our {@link LocalEntityResolver} will prevent the usage of the
+     * file URL scheme protocol.
+     */
+    @Test
+    void preventXXEAttack() throws Exception
+    {
+        SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        SAXParser parser = parserFactory.newSAXParser();
+        XMLReader xmlReader = parser.getXMLReader();
+        xmlReader.setEntityResolver(new LocalEntityResolver());
+        String input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<!DOCTYPE foo [ <!ENTITY xxe SYSTEM \"file:///etc/passwd\"> ]>\n"
+            + "<p>&xxe;</p>";
+        InputSource source = new InputSource(new ByteArrayInputStream(input.getBytes()));
+
+        Throwable exception = assertThrows(SAXException.class, () -> {
+            xmlReader.parse(source);
+        });
+        assertEquals("Invalid url protocol: file", exception.getMessage());
+    }
+}
