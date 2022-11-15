d679cab5d9354ceb403fd552c00c4ab7170d70d6
Michael Hamann <michael.hamann@xwiki.com>
2022-11-15 15:29:29 +0100

    XCOMMONS-2568: Improve comment handling in HTMLCleaner
    * Added an explanation why comments are removed in restricted mode

--- xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/DefaultHTMLCleaner.java
+++ xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/DefaultHTMLCleaner.java
@@ -268,6 +268,10 @@ public class DefaultHTMLCleaner implements HTMLCleaner
         defaultProperties.setDeserializeEntities(true);
 
         // Omit comments in restricted mode to avoid any potential parser confusion.
+        // Any part of the filtered HTML that contains unfiltered input is potentially dangerous/a candidate for
+        // parser confusion. Comments, style and script elements seem to be frequently found ingredients in successful
+        // attacks against good sanitizers. We're already removing style and script elements, so removing comments
+        // seems like a good defense against future attacks.
         defaultProperties.setOmitComments(isRestricted(configuration));
 
         return defaultProperties;
