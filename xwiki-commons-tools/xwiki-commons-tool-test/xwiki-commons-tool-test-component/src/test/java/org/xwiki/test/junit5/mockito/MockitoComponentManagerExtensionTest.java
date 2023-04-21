a410f3dc566a84ebeda62302db29dfaf12ecb45d
Thomas Mortagne <thomas.mortagne@gmail.com>
2023-04-21 10:18:54 +0200

    XCOMMONS-2507: Add a priority concept in components * add missing import

--- xwiki-commons-tools/xwiki-commons-tool-test/xwiki-commons-tool-test-component/src/test/java/org/xwiki/test/junit5/mockito/MockitoComponentManagerExtensionTest.java
+++ xwiki-commons-tools/xwiki-commons-tool-test/xwiki-commons-tool-test-component/src/test/java/org/xwiki/test/junit5/mockito/MockitoComponentManagerExtensionTest.java
@@ -37,6 +37,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
