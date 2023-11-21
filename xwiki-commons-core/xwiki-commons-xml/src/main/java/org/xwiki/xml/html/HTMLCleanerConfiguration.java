9563cfd4cd03546505aeecd27b22214b71ff1e67
Manuel Leduc <manuel.leduc@xwiki.com>
2023-11-21 14:59:51 +0100

    [Misc] More removal of @Unstable annotations for APIs that are now considered stable
    Following XCOMMONS-2861 and XCOMMONS-2860.

--- xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/html/HTMLCleanerConfiguration.java
+++ xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/html/HTMLCleanerConfiguration.java
@@ -22,7 +22,6 @@ package org.xwiki.xml.html;
 import java.util.List;
 import java.util.Map;
 
-import org.xwiki.stability.Unstable;
 import org.xwiki.xml.html.filter.HTMLFilter;
 
 /**
@@ -59,7 +58,6 @@ public interface HTMLCleanerConfiguration
      * The HTML (major) version. Should be "5" for HTML5 and "4" (default) otherwise for the default implementation.
      * @since 14.0RC1
      */
-    @Unstable
     String HTML_VERSION = "htmlVersion";
 
     /**
