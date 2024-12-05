666bd3df4f181fdc889dc1967e28288538612dbe
Vincent Massol <vincent@massol.net>
2024-12-05 10:15:55 +0100

    [Misc] No need to call "toString()" method as formatting and string conversion is done by the Formatter (found by SonarQube)

--- xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/repository/internal/local/LocalExtensionStorage.java
+++ xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-api/src/main/java/org/xwiki/extension/repository/internal/local/LocalExtensionStorage.java
@@ -339,7 +339,7 @@ public class LocalExtensionStorage
             deleteExtensionFolderIfEmpty(extensionVersionFolderPath.getParent());
         } catch (DirectoryNotEmptyException e) {
             LOGGER.warn("Extension version folder [{}] was not empty after removing the extension [{}]. Keeping it.",
-                extensionDescriptorFilePath.toString(), extension.getId().getId());
+                extensionDescriptorFilePath, extension.getId().getId());
         }
     }
 
@@ -356,7 +356,7 @@ public class LocalExtensionStorage
         } catch (DirectoryNotEmptyException e) {
             // Folder not being empty is a valid scenario if some version of the extension are still installed
             LOGGER.debug("Extension folder [{}] was not empty after removing the extension. Keeping it.",
-                extensionFolderPath.toString());
+                extensionFolderPath);
         }
     }
 }
