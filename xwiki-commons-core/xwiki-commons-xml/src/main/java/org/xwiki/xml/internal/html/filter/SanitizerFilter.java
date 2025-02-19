23f854fc8ed753906fabc21105040ac8f3a575c4
Michael Hamann <michael@content-space.de>
2025-02-19 10:52:25 +0100

    [Misc] Clarify description of the invalid TagInformation

--- xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/filter/SanitizerFilter.java
+++ xwiki-commons-core/xwiki-commons-xml/src/main/java/org/xwiki/xml/internal/html/filter/SanitizerFilter.java
@@ -85,7 +85,7 @@ public class SanitizerFilter extends AbstractHTMLFilter
     protected static class TagInformation
     {
         /**
-         * Placeholder for an invalid tag.
+         * Placeholder for a tag that is either unknown or used in the wrong namespace.
          */
         public static final TagInformation INVALID = new TagInformation(null, null);
 
