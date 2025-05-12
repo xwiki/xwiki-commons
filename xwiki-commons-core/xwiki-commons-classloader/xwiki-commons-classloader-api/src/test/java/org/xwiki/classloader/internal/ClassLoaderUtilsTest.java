020854f13ed4e0c4715905754e6e1e454e0bbb03
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-05-12 16:23:31 +0200

    XCOMMONS-3332: Issues when creating and running an instance of XWiki 16.10.7 on Windows

--- xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/test/java/org/xwiki/classloader/internal/ClassLoaderUtilsTest.java
+++ xwiki-commons-core/xwiki-commons-classloader/xwiki-commons-classloader-api/src/test/java/org/xwiki/classloader/internal/ClassLoaderUtilsTest.java
@@ -46,6 +46,8 @@ class ClassLoaderUtilsTest
 
     private static final String RESOURCE_NAME_BACK = "folder/../" + RESOURCE_NAME;
 
+    private static final String PREFIXED_RESOURCE_NAME_BACK = PREFIX + RESOURCE_NAME_BACK;
+
     private ClassLoader classLoader;
 
     private URL resouceURL;
@@ -64,12 +66,16 @@ class ClassLoaderUtilsTest
         this.resouceURL = new URL("http://host");
         this.prefixedResourceURL = new URL("http://host");
         when(this.classLoader.getResource(RESOURCE_NAME)).thenReturn(this.resouceURL);
+        when(this.classLoader.getResource(RESOURCE_NAME_BACK)).thenReturn(this.resouceURL);
         when(this.classLoader.getResource(PREFIXED_RESOURCE_NAME)).thenReturn(this.prefixedResourceURL);
+        when(this.classLoader.getResource(PREFIXED_RESOURCE_NAME_BACK)).thenReturn(this.prefixedResourceURL);
 
         this.inputStream = mock();
         this.prefixedInputStream = mock();
         when(this.classLoader.getResourceAsStream(RESOURCE_NAME)).thenReturn(this.inputStream);
+        when(this.classLoader.getResourceAsStream(RESOURCE_NAME_BACK)).thenReturn(this.inputStream);
         when(this.classLoader.getResourceAsStream(PREFIXED_RESOURCE_NAME)).thenReturn(this.prefixedInputStream);
+        when(this.classLoader.getResourceAsStream(PREFIXED_RESOURCE_NAME_BACK)).thenReturn(this.prefixedInputStream);
     }
 
     @Test
