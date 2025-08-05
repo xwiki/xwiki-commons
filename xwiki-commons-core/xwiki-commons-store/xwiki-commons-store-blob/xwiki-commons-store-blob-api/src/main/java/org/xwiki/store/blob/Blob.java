87c3a8c301c52b1e4901b189cc9af60d5f746848
Michael Hamann <michael.hamann@xwiki.com>
2025-08-05 18:21:00 +0200

    XCOMMONS-3250: Make as easy as possible for various features to use an object store
    * Extended the blog store API a bit.
    * Added an S3 implementation, the initial draft was by Claude Sonnet 4.
      Untested.

--- xwiki-commons-core/xwiki-commons-store/xwiki-commons-store-blob/xwiki-commons-store-blob-api/src/main/java/org/xwiki/store/blob/Blob.java
+++ xwiki-commons-core/xwiki-commons-store/xwiki-commons-store-blob/xwiki-commons-store-blob-api/src/main/java/org/xwiki/store/blob/Blob.java
@@ -21,7 +21,6 @@ package org.xwiki.store.blob;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Path;
 
 import org.xwiki.stability.Unstable;
 import org.xwiki.store.StreamProvider;
@@ -43,12 +42,13 @@ public interface Blob extends StreamProvider
     /**
      * @return the path of this blob inside its store
      */
-    Path getPath();
+    BlobPath getPath();
 
     /**
      * @return true if the blob exists, false otherwise
+     * @throws BlobStoreException when the existence of the blob cannot be determined
      */
-    boolean exists();
+    boolean exists() throws BlobStoreException;
 
     /**
      * Get the size of this blob.
@@ -59,21 +59,33 @@ public interface Blob extends StreamProvider
     long getSize() throws BlobStoreException;
 
     /**
+     * @param conditions the conditions that must be satisfied before writing to this blob
      * @return an OutputStream to write data to this blob
      * @throws BlobStoreException if the blob cannot be written, for example because its name is invalid. There is no
      *     guarantee that in such a case an exception will be thrown, the exception could also only be thrown when data
      *     is written to the stream, or when the stream is closed.
      */
-    OutputStream getOutputStream() throws BlobStoreException;
+    OutputStream getOutputStream(WriteCondition... conditions) throws BlobStoreException;
 
     /**
      * Write the content of the given InputStream to this blob.
      *
      * @param inputStream the InputStream to read data from
+     * @param conditions the conditions that must be satisfied before writing to this blob
      * @throws BlobStoreException if the InputStream cannot be read or the blob cannot be written, for example because
      * its name is invalid.
      */
-    void writeFromStream(InputStream inputStream) throws BlobStoreException;
+    void writeFromStream(InputStream inputStream, WriteCondition... conditions) throws BlobStoreException;
+
+    /**
+     * Get an InputStream to read data from this blob.
+     *
+     * @return an InputStream to read data from this blob
+     * @throws Exception if the blob cannot be read
+     * @throws BlobNotFoundException if the blob does not exist
+     */
+    @Override
+    InputStream getStream() throws Exception;
 
     /**
      * Copy this blob to another path within the same store.
@@ -81,8 +93,10 @@ public interface Blob extends StreamProvider
      * @param targetPath the target path for the copy
      * @return the new blob at the target path
      * @throws BlobStoreException if the copy operation fails, for example because this blob doesn't exist
+     * @throws BlobNotFoundException if this blob does not exist
+     * @throws BlobAlreadyExistsException if a blob already exists at the target path
      */
-    default Blob copyTo(Path targetPath) throws BlobStoreException
+    default Blob copyTo(BlobPath targetPath) throws BlobStoreException
     {
         return getStore().copyBlob(getPath(), targetPath);
     }
