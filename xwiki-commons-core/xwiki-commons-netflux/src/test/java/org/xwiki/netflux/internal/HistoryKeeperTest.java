b58901796481a6cf6e92cea4c09d24dc9f99588b
Marius Dumitru Florea <marius@xwiki.com>
2025-09-23 10:07:04 +0300

    XCOMMONS-3425: The Netflux WebSocket endpoint delays replies for no good reason
    (cherry picked from commit 782676bda99bfd3d3d767b3ca361ae33d93dd428)

--- xwiki-commons-core/xwiki-commons-netflux/src/test/java/org/xwiki/netflux/internal/HistoryKeeperTest.java
+++ xwiki-commons-core/xwiki-commons-netflux/src/test/java/org/xwiki/netflux/internal/HistoryKeeperTest.java
@@ -19,18 +19,22 @@
  */
 package org.xwiki.netflux.internal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.util.List;
+
+import jakarta.websocket.RemoteEndpoint.Basic;
+import jakarta.websocket.Session;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.xwiki.test.annotation.ComponentList;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Unit tests for {@link HistoryKeeper}.
@@ -48,15 +52,23 @@ class HistoryKeeperTest
     private ChannelStore channels;
 
     @MockComponent
-    private MessageDispatcher dispatcher;
+    private MessageBuilder messageBuilder;
 
-    private User user = new User(null, "alice");
+    @Mock
+    Session session;
+
+    @Mock
+    Basic basicRemote;
+
+    private User user;
 
     private Channel channel = new Channel("test");
 
     @BeforeEach
     void beforeEach()
     {
+        when(this.session.getBasicRemote()).thenReturn(this.basicRemote);
+        this.user = new User(this.session, "alice");
         when(this.channels.get("test")).thenReturn(this.channel);
     }
 
@@ -67,30 +79,41 @@ class HistoryKeeperTest
     }
 
     @Test
-    void onUserMessage()
+    void onUserMessage() throws Exception
     {
         // Message size too small.
         this.historyKeeper.onUserMessage(this.user, List.of(0, 1, 2));
 
         // Invalid message.
-        when(this.dispatcher.decode("invalid")).thenThrow(new RuntimeException());
+        when(this.messageBuilder.decode("invalid")).thenThrow(new RuntimeException());
         this.historyKeeper.onUserMessage(this.user, List.of(0, "MSG", this.historyKeeper.getId(), "invalid"));
 
         // Unknown message type.
-        when(this.dispatcher.decode("valid")).thenReturn(List.of("GET_TIME", "test"));
+        when(this.messageBuilder.decode("valid")).thenReturn(List.of("GET_TIME", "test"));
         this.historyKeeper.onUserMessage(this.user, List.of(0, "MSG", this.historyKeeper.getId(), "valid"));
 
+        // Get history without specifying the channel.
+        when(this.messageBuilder.decode("valid")).thenReturn(List.of("GET_HISTORY"));
+        this.historyKeeper.onUserMessage(this.user, List.of(0, "MSG", this.historyKeeper.getId(), "valid"));
+
+        // Get history for missing channel.
+        when(this.messageBuilder.decode("valid")).thenReturn(List.of("GET_HISTORY", "missing"));
+        when(this.messageBuilder.buildMessage(0, this.historyKeeper.getId(), this.user.getName(),
+            "{\"state\":1, \"channel\":\"missing\"}")).thenReturn("end missing");
+        this.historyKeeper.onUserMessage(this.user, List.of(0, "MSG", this.historyKeeper.getId(), "valid"));
+        verify(this.basicRemote).sendText("end missing");
+
         // Get history message.
-        when(this.dispatcher.decode("valid")).thenReturn(List.of("GET_HISTORY", "test"));
+        when(this.messageBuilder.decode("valid")).thenReturn(List.of("GET_HISTORY", "test"));
         this.channel.getMessages().addAll(List.of("first", "second"));
-        when(this.dispatcher.buildMessage(0, this.historyKeeper.getId(), this.user.getName(),
+        when(this.messageBuilder.buildMessage(0, this.historyKeeper.getId(), this.user.getName(),
             "{\"state\":1, \"channel\":\"test\"}")).thenReturn("end");
 
         this.historyKeeper.onUserMessage(this.user, List.of(0, "MSG", this.historyKeeper.getId(), "valid"));
 
-        verify(this.dispatcher).addMessage(this.user, "first");
-        verify(this.dispatcher).addMessage(this.user, "second");
-        verify(this.dispatcher).addMessage(this.user, "end");
+        verify(this.basicRemote).sendText("first");
+        verify(this.basicRemote).sendText("second");
+        verify(this.basicRemote).sendText("end");
     }
 
     @Test
@@ -104,17 +127,17 @@ class HistoryKeeperTest
         assertEquals(List.of("Hi!", "Anyone here?"), this.channel.getMessages());
 
         // Simulate a checkpoint message.
-        when(this.dispatcher.decode("checkpoint")).thenReturn(List.of(1, "cp|[4,[test"));
+        when(this.messageBuilder.decode("checkpoint")).thenReturn(List.of(1, "cp|[4,[test"));
         this.historyKeeper.onChannelMessage(this.channel, this.user, "MSG", "checkpoint");
 
         // Add another message that looks like a checkpoint message but it's not.
-        when(this.dispatcher.decode("fake checkpoint")).thenReturn(List.of("cp|[4,[test", 1));
+        when(this.messageBuilder.decode("fake checkpoint")).thenReturn(List.of("cp|[4,[test", 1));
         this.historyKeeper.onChannelMessage(this.channel, this.user, "MSG", "fake checkpoint");
 
         assertEquals(List.of("Hi!", "Anyone here?", "checkpoint", "fake checkpoint"), this.channel.getMessages());
 
         // Simulate another checkpoint message to test message pruning.
-        when(this.dispatcher.decode("another checkpoint")).thenReturn(List.of("cp|[4,[end"));
+        when(this.messageBuilder.decode("another checkpoint")).thenReturn(List.of("cp|[4,[end"));
         this.historyKeeper.onChannelMessage(this.channel, this.user, "MSG", "another checkpoint");
 
         assertEquals(List.of("checkpoint", "fake checkpoint", "another checkpoint"), this.channel.getMessages());
@@ -123,7 +146,7 @@ class HistoryKeeperTest
         this.historyKeeper.onChannelMessage(this.channel, this.user, "MSG", "Bye!");
 
         // Simulate a final checkpoint message to test message pruning.
-        when(this.dispatcher.decode("final checkpoint")).thenReturn(List.of(1, "foo", "cp|[4,[final"));
+        when(this.messageBuilder.decode("final checkpoint")).thenReturn(List.of(1, "foo", "cp|[4,[final"));
         this.historyKeeper.onChannelMessage(this.channel, this.user, "MSG", "final checkpoint");
 
         assertEquals(List.of("another checkpoint", "I'm alone", "Bye!", "final checkpoint"),
