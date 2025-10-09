628afabe09b0dc82e9e4ae6674674949218aedcd
Manuel Leduc <manuel.leduc@xwiki.com>
2025-10-09 10:17:41 +0200

    [Misc] Migrate xwiki-commons-crypto-password to Junit 5

--- xwiki-commons-core/xwiki-commons-crypto/xwiki-commons-crypto-password/src/test/java/org/xwiki/crypto/password/internal/kdf/factory/DefaultKeyDerivationFunctionFactoryTest.java
+++ xwiki-commons-core/xwiki-commons-crypto/xwiki-commons-crypto-password/src/test/java/org/xwiki/crypto/password/internal/kdf/factory/DefaultKeyDerivationFunctionFactoryTest.java
@@ -21,53 +21,43 @@ package org.xwiki.crypto.password.internal.kdf.factory;
 
 import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.util.encoders.Hex;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.xwiki.crypto.params.cipher.symmetric.KeyWithIVParameters;
 import org.xwiki.crypto.password.KeyDerivationFunction;
-import org.xwiki.crypto.password.KeyDerivationFunctionFactory;
 import org.xwiki.crypto.password.PasswordToByteConverter;
 import org.xwiki.crypto.password.params.KeyDerivationFunctionParameters;
 import org.xwiki.crypto.password.params.PBKDF2Parameters;
 import org.xwiki.crypto.password.params.ScryptParameters;
 import org.xwiki.test.annotation.ComponentList;
-import org.xwiki.test.mockito.MockitoComponentMockingRule;
+import org.xwiki.test.junit5.mockito.ComponentTest;
+import org.xwiki.test.junit5.mockito.InjectMockComponents;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@ComponentList({BcScryptKeyDerivationFunctionFactory.class, BcPKCS5S2KeyDerivationFunctionFactory.class})
-public class DefaultKeyDerivationFunctionFactoryTest
+@ComponentTest
+@ComponentList({ BcScryptKeyDerivationFunctionFactory.class, BcPKCS5S2KeyDerivationFunctionFactory.class })
+class DefaultKeyDerivationFunctionFactoryTest
 {
-    @Rule
-    public final MockitoComponentMockingRule<KeyDerivationFunctionFactory> mocker =
-        new MockitoComponentMockingRule<>(DefaultKeyDerivationFunctionFactory.class);
+    @InjectMockComponents
+    private DefaultKeyDerivationFunctionFactory factory;
 
-    KeyDerivationFunctionFactory factory;
-
-    @Before
-    public void configure() throws Exception
+    private KeyDerivationFunction getKDFInstance(KeyDerivationFunctionParameters parameters)
     {
-        factory = mocker.getComponentUnderTest();
-    }
-
-    KeyDerivationFunction getKDFInstance(KeyDerivationFunctionParameters parameters)
-    {
-        return factory.getInstance(parameters);
+        return this.factory.getInstance(parameters);
     }
 
     @Test
-    public void Pbkdf2DecodingTest() throws Exception
+    void pbkdf2DecodingTest() throws Exception
     {
         byte[] password = PasswordToByteConverter.convert("password");
         byte[] encoded = Base64.decode("MCYGCSqGSIb3DQEFDDAZBBAcO15L0rQ01O2RJ5UhyqCdAgID6AIBIA==");
 
-        KeyDerivationFunction kdf = getKDFInstance(new PBKDF2Parameters(32, 1000, Hex.decode(
-            "1c3b5e4bd2b434d4ed91279521caa09d")));
+        KeyDerivationFunction kdf =
+            getKDFInstance(new PBKDF2Parameters(32, 1000, Hex.decode("1c3b5e4bd2b434d4ed91279521caa09d")));
         KeyWithIVParameters params = kdf.derive(password, 8);
 
-        KeyDerivationFunction kdf2 = factory.getInstance(encoded);
+        KeyDerivationFunction kdf2 = this.factory.getInstance(encoded);
         KeyWithIVParameters params2 = kdf2.derive(password, 8);
 
         assertThat(kdf.getEncoded(), equalTo(encoded));
@@ -76,15 +66,16 @@ public class DefaultKeyDerivationFunctionFactoryTest
     }
 
     @Test
-    public void ScryptDecodingTest() throws Exception
+    void scryptDecodingTest() throws Exception
     {
         byte[] password = PasswordToByteConverter.convert("password");
         byte[] encoded = Base64.decode("MCwGCSsGAQQB2kcECzAfBBAcO15L0rQ01O2RJ5UhyqCdAgICAAIBEAIBAgIBIA==");
 
-        KeyDerivationFunction kdf = getKDFInstance(new ScryptParameters(32, 512, 2, 16, Hex.decode("1c3b5e4bd2b434d4ed91279521caa09d")));
+        KeyDerivationFunction kdf =
+            getKDFInstance(new ScryptParameters(32, 512, 2, 16, Hex.decode("1c3b5e4bd2b434d4ed91279521caa09d")));
         KeyWithIVParameters params = kdf.derive(password, 8);
 
-        KeyDerivationFunction kdf2 = factory.getInstance(encoded);
+        KeyDerivationFunction kdf2 = this.factory.getInstance(encoded);
         KeyWithIVParameters params2 = kdf2.derive(password, 8);
 
         assertThat(kdf.getEncoded(), equalTo(encoded));
