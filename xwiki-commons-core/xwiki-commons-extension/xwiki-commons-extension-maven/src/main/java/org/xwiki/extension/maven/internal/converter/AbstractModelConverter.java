f8540cc5783f1f8ec993e1fe77d7f18d4404607c
Manuel Leduc <manuel.leduc@xwiki.com>
2026-03-05 09:39:33 +0100

    [Misc] Replace use of deprecated StringUtils.defaultString(java.lang.String, java.lang.String) with Objects.toString(java.lang.Object, java.lang.String)
    (cherry picked from commit 3b2fccbb285ef45609f113d3b56e98ced2cc9ede)

--- xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-maven/src/main/java/org/xwiki/extension/maven/internal/converter/AbstractModelConverter.java
+++ xwiki-commons-core/xwiki-commons-extension/xwiki-commons-extension-maven/src/main/java/org/xwiki/extension/maven/internal/converter/AbstractModelConverter.java
@@ -27,6 +27,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -299,7 +300,7 @@ public class AbstractModelConverter<T> extends AbstractConverter<T>
 
     private String getPropertyString(Properties properties, String propertyName, boolean delete, String def)
     {
-        return StringUtils.defaultString(getProperty(properties, propertyName, delete), def);
+        return Objects.toString(getProperty(properties, propertyName, delete), def);
     }
 
     // TODO: download custom licenses content
